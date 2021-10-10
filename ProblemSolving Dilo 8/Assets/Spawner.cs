0f4a788c18df8e7e3a66fea20626d64fc50648f4
DESKTOP-0283LTQ\darkc <dendi.stefanus@binus.ac.id>
2021-10-10 18:58:02 +0700

    8

---
+++ ProblemSolving Dilo 8/Assets/Spawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    int i = 0;
+    bool spawning= true;
+    public GameObject[] scores;
+    int totalScores =4;
+    public Vector3 Screen;
+    private GameObject Squares;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        InvokeRepeating("Spawn", 3.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    public void Spawn()
+    {
+        if(spawning == false)
+        {
+            StartCoroutine(WaitandSpawn(3f));
+        }
+        
+        float RandomX = Random.Range(-3.8f, 4.2f);
+            float RandomY = Random.Range(-2.7f, 3.4f);
+            Instantiate(scores[i], new Vector2(RandomX, RandomY), Quaternion.identity);
+        if(i<totalScores)
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+            
+        }
+        
+        spawning = false;
+        
+    }
+    private IEnumerator WaitandSpawn(float wait)
+    {
+        
+        yield return new WaitForSeconds(wait);
+        spawning=true;
+    }
+}
