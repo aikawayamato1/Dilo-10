a49dfc3b8d84b0802a87c0cb38997afe5046e39a
DESKTOP-0283LTQ\darkc <dendi.stefanus@binus.ac.id>
2021-10-10 18:56:07 +0700

    7

---
+++ ProblemSolving Dilo 7/Assets/PlayerScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    public int score;
+    public void addScore()
+    {
+        score++;
+    }
+    
+}
